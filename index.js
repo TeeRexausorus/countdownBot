baab71b534739df0fb11bc2a933713e064cab99d
dextraise <gilles@teerex.ovh>
2021-01-05 17:42:18 +0100

    adding random coloring... With a twist

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const TOKEN = process.env.TOKEN;
 
 const countDownDate = new Date("Mar 20, 2021 12:00:00").getTime();
 
-function getCountdownAsString() {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getCountdownAsString(blockedVal) {
     // Get today's date and time
     let now = new Date().getTime() + (1000 * 60 * 60);
 
@@ -18,9 +22,16 @@ function getCountdownAsString() {
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    return ('```ini' + '\n[Les mariages auront lieu dans ' + days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes, et ' + seconds + 'secondes.]\n' +
-        '```');
+    let randomVal = blockedVal > 0 ? blockedVal : getRandomInt(2);
+    console.log(randomVal);
+    switch(randomVal){
+        case 0:
+            return ('```ini' + '\n[Les mariages auront lieu dans ' + days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes, et ' + seconds + 'secondes.]\n```');
+        case 1:
+            return ('```diff' + '\n- Les mariages auront lieu dans ' + days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes, et ' + seconds + 'secondes.\n```');
+        default:
+            return ('```diff' + '\n- Les mariages auront lieu dans ' + days + ' jours, ' + hours + ' heures, ' + minutes + ' minutes, et ' + seconds + 'secondes.\n```');
+    }
 }
 
 bot.login(TOKEN);
@@ -31,6 +42,11 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
     if (message.content === '!countdown') {
-        message.channel.send(getCountdownAsString());
+        if(message.author.username === 'Blue'){
+            message.channel.send(getCountdownAsString(0));
+        }
+        else{
+            message.channel.send(getCountdownAsString());
+        }
     }
 });
